e716793c21bb64c509a9cd6d02b3d51138ead09c
MrConnorKenway <auriaiur@outlook.com>
2025-05-17 11:41:07 +0800

    build: zig build system

---
+++ main.zig
@@ -0,0 +1,8 @@
+pub usingnamespace @cImport({
+    @cInclude("libssh/libssh.h");
+    @cInclude("libssh/server.h");
+    @cInclude("libssh/ssh2.h");
+    @cInclude("libssh/callbacks.h");
+    @cInclude("libssh/sftp.h");
+    @cInclude("libssh/sftpserver.h");
+});
