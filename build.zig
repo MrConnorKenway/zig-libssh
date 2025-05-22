8508495c980c1241c11d60a2ec854e40357197e7
MrConnorKenway <auriaiur@outlook.com>
2025-05-23 01:07:55 +0800

    Generate cmake build folder under current working directory

--- build.zig
+++ build.zig
@@ -26,22 +26,30 @@ pub fn build(b: *std.Build) !void {
     // Get the real path of libssh we depend on and call cmake under its build
     // root path to generate `compile_commands.json`.
     const upstream = b.dependency("libssh", .{});
-    var build_root_path = upstream.builder.build_root.path.?;
-    const build_root = try std.fs.openDirAbsolute(build_root_path, .{});
-    build_root_path = try build_root.realpathAlloc(allocator, ".");
-    defer allocator.free(build_root_path);
-    build_root.access("build/compile_commands.json", .{}) catch |err| switch (err) {
+    var upstream_root_str = upstream.builder.build_root.path.?;
+    const upstream_root_dir = try std.fs.openDirAbsolute(upstream_root_str, .{});
+    upstream_root_str = try upstream_root_dir.realpathAlloc(allocator, ".");
+    defer allocator.free(upstream_root_str);
+    const current_build_dir = try std.fs.openDirAbsolute(b.build_root.path.?, .{});
+    const current_build_path = try current_build_dir.realpathAlloc(allocator, ".");
+    defer allocator.free(current_build_path);
+    current_build_dir.access("build/compile_commands.json", .{}) catch |err| switch (err) {
         error.FileNotFound => {
             const argv = &[_][]const u8{
                 "cmake",
                 "-B",
                 "build",
                 "-DCMAKE_EXPORT_COMPILE_COMMANDS=1",
+                "-S",
+                upstream_root_str,
             };
+            const argv_str = try std.mem.join(allocator, " ", argv);
+            defer allocator.free(argv_str);
+            std.log.info("{s}\n", .{argv_str});
             const result = try std.process.Child.run(.{
                 .allocator = allocator,
                 .argv = argv,
-                .cwd = build_root_path,
+                .cwd = current_build_path,
             });
             defer allocator.free(result.stdout);
             defer allocator.free(result.stderr);
@@ -64,7 +72,7 @@ pub fn build(b: *std.Build) !void {
     };
 
     // Try to open and read compile_commands.json
-    const file = try build_root.openFile("build/compile_commands.json", .{});
+    const file = try current_build_dir.openFile("build/compile_commands.json", .{});
     defer file.close();
 
     const file_size = try file.getEndPos();
@@ -93,20 +101,20 @@ pub fn build(b: *std.Build) !void {
                 }
                 try flags.append(flag);
             }
-            if (!std.mem.startsWith(u8, entry.file, build_root_path)) {
-                const msg = try std.fmt.allocPrint(allocator, "UnexpectedFilePath: file '{s}' is not under cwd '{s}'", .{ entry.file, build_root_path });
+            if (!std.mem.startsWith(u8, entry.file, upstream_root_str)) {
+                const msg = try std.fmt.allocPrint(allocator, "UnexpectedFilePath: file '{s}' is not under cwd '{s}'", .{ entry.file, upstream_root_str });
                 @panic(msg);
             }
             module.addCSourceFiles(.{
                 .root = upstream.path(""),
-                .files = &.{entry.file[build_root_path.len + 1 ..]},
+                .files = &.{entry.file[upstream_root_str.len + 1 ..]},
                 .flags = flags.items,
             });
         }
     }
 
     module.addIncludePath(upstream.path("include"));
-    module.addIncludePath(upstream.path("build/include"));
+    module.addIncludePath(b.path("build/include"));
 
     module.linkSystemLibrary("crypto", .{});
     module.linkSystemLibrary("ssl", .{});
